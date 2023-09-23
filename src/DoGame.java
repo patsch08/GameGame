92027d7e2bb9082a47896c86432c9b7afdf2845e
patsch08 <patsch2008@gmail.com>
2023-09-23 18:16:21 +0200

    Changed the Player class and added selectionSort, printHand, has which checks if the player has the given card,  added doTrade method which is not finished, improved choice class which is probably finished, added a char array called action the first part is the player initiating the trade the second part is the player getting traded with and the third part is the animal being traded after each successful trade action gets wiped and is only again initiated using the doTrade and choice methods

--- src/DoGame.java
+++ src/DoGame.java
@@ -9,78 +9,107 @@ public class DoGame {
     private static Deck deck = new Deck();
     private static int[] action = new int[3];
     private static Card card = deck.draw();
-    public static void doGame(int player){
-        if(player > 0&&5 >= player){
-            players = new Player[player];
+    public static void doGame(int playerCount) {
+        // Initialize the game with the specified number of players
+        if (playerCount < 1 || playerCount > 5) {
+            System.out.println("Invalid number of players. Setting players to 3.");
+            playerCount = 3;
         }
-        else {
-            System.out.println("Invalid Player Input players set to 3");
+        players = new Player[playerCount];
+        for (int i = 0; i < playerCount; i++) {
+            players[i] = new Player("Player " + (i + 1));
         }
-        while(true){
+
+        boolean gameRunning = true;
+
+        while (gameRunning) {
             int decision = choice();
-            if (decision == -1)continue;
-            if(decision == 0){
-                while(true){
+            if (decision == -1) {
+                continue;
+            }
+            if (decision == 0) {
+                while (true) {
                     int doA = doAuction();
-                    if(doA == -1)continue;
-                    else players[doA].addCard(card);
-                    card = deck.draw();
+                    if (doA == -1) {
+                        continue;
+                    } else {
+                        players[doA].addCard(card);
+                        card = deck.draw();
+                    }
                 }
             }
+            if(deck.isEmpty())gameRunning = false;
         }
     }
-    public static int doAuction(){
+
+    public static int doAuction() {
         ArrayList<Integer> exclude = new ArrayList<>();
-        exclude.add(current-1);
+        exclude.add(current - 1);
         int highestBid = 0;
-        int highestBidder= current-1;
-        for(int i=0;true;i++){
-            while(true) {
-                if (exclude.contains(i)) {
-                    i++;
-                    if (i == players.length) {
-                        i = 0;
-                    }
+        int highestBidder = current - 1;
+
+        for (int i = 0; exclude.size() < players.length - 1; i++) {
+            if (exclude.contains(i)) {
+                i++;
+                if (i >= players.length) {
+                    i = 0;
                 }
-                else break;
             }
+            if (i >= players.length) {
+                i = 0;
+            }
+            if (exclude.contains(i)) {
+                continue;
+            }
+
             System.out.println(card.animal + " " + card.value);
-            System.out.printf("Player %d how much do you want to bid?\n",i);
+            System.out.printf("Player %d how much do you want to bid? (0 to exit)\n", i + 1);
             int bid = 0;
-            while(true){
-                System.out.println("> ");
-                if (scanner.hasNextInt()){
+            boolean playerExited = false; // Flag to track if the player exits
+
+            while (true) {
+                System.out.print("> ");
+                if (scanner.hasNextInt()) {
                     bid = scanner.nextInt();
-                    //checks if bid is 0 then excludes him from further bidding
-                    if (bid == 0){
-                        System.out.println("Youre out!");
+                    if (bid == 0) {
+                        System.out.println("Player " + (i + 1) + " is out of the auction!");
                         exclude.add(i);
+                        playerExited = true; // Set the flag to indicate player exit
+                        break;
                     }
-                    if(bid > highestBid){
-                        System.out.println("You have the lead");
+                    if (bid > highestBid) {
+                        System.out.printf("Player %d has the lead\n",i+1);
                         highestBidder = i;
                         highestBid = bid;
                     }
                     break;
+                } else {
+                    scanner.next();
                 }
             }
-            if(exclude.size() == players.length-1)break;
+
+            if (exclude.size() == players.length - 1 || playerExited) {
+                break; // Exit the loop when a player exits or when all players are excluded
+            }
         }
-        if(highestBid < players[highestBidder].getTotal()){
+
+        if (highestBid < players[highestBidder].getTotal()) {
             int whole = 0;
-            while(whole < 50){
+            while (whole < 50) {
                 int has50 = players[highestBidder].hasMoney(3);
-                if (has50 != -1){
+                if (has50 != -1) {
                     players[highestBidder].removeMoney(3, 1);
                     break;
                 }
-                players[highestBidder].removeMoney(1,1);
+                players[highestBidder].removeMoney(1, 1);
                 whole += 10;
             }
             return -1;
         }
+        System.out.printf("Player %d has won the auction!\n He now has a %s",highestBidder,card.getAnimal());
         return highestBidder;
     }
+
     public static int choice(){
         System.out.printf("Player %d what do you want to do?\n[0] Do you want to draw a card\n[Player with who you want to trade] Do you want to trade\n",current);
         int input;
@@ -104,8 +133,7 @@ public class DoGame {
         }
         return -1;
     }
-    public static boolean doTrade(){
-        System.out.println("Which animal do you want to trade?");
-        return true;
-    }
+
+    
+
 }
